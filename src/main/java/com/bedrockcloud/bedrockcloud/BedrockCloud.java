f30f990581802279c2fca753a28704e64fc2c6b5
Florian <38558911+WhitqFlorian@users.noreply.github.com>
2022-10-12 09:08:18 +0200

    update

--- src/main/java/com/bedrockcloud/bedrockcloud/BedrockCloud.java
+++ src/main/java/com/bedrockcloud/bedrockcloud/BedrockCloud.java
@@ -88,6 +88,52 @@ public class BedrockCloud
 
         this.startAllProxies();
         BedrockCloud.networkManager.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            BedrockCloud.setRunning(false);
+            boolean allServicesStopped = false;
+            do {
+                try {
+                    if (BedrockCloud.networkManager.serverSocket != null && !BedrockCloud.networkManager.serverSocket.isClosed()){
+                        BedrockCloud.networkManager.serverSocket.close();
+                        BedrockCloud.getLogger().warning("§cServerSocket was closed.");
+                    }
+                } catch (IOException ignored) {}
+
+                for (final String templateName : BedrockCloud.getTemplateProvider().templateMap.keySet()) {
+                    BedrockCloud.getTemplateProvider().removeRunningGroup(templateName);
+                }
+
+                for (final GameServer gameServer : BedrockCloud.getGameServerProvider().gameServerMap.values()) {
+                    gameServer.stopServer();
+                }
+
+                for (final String proxy : BedrockCloud.getProxyServerProvider().proxyServerMap.keySet()) {
+                    final ProxyServer proxyServer = BedrockCloud.getProxyServerProvider().getProxyServer(proxy);
+                    proxyServer.stopServer();
+                }
+
+                int gameServerCount = BedrockCloud.getGameServerProvider().gameServerMap.size();
+                int privateServerCount = BedrockCloud.getPrivateGameServerProvider().gameServerMap.size();
+                int proxyServerCount = BedrockCloud.getProxyServerProvider().proxyServerMap.size();
+
+                if (gameServerCount <= 0 && privateServerCount <= 0 && proxyServerCount <= 0){
+                    allServicesStopped = true;
+                    BedrockCloud.getLogger().info("§aAll services were stopped.");
+                }
+
+                final ProcessBuilder builder = new ProcessBuilder();
+                try {
+                    if (allServicesStopped) {
+                        Thread.sleep(2000);
+                        builder.command("/bin/sh", "-c", "killall -9 php").start();
+                        builder.command("/bin/sh", "-c", "killall -9 java").start();
+                    }
+                } catch (IOException | InterruptedException e) {
+                    BedrockCloud.getLogger().exception(e);
+                }
+            } while (!BedrockCloud.isRunning());
+        }));
     }
 
     public static boolean isRunning() {
